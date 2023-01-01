a6599c471df9924f33d35efd7793508d1c0574de
subrat66 <subratku.gaana66@gmail.com>
2023-01-02 01:21:52 +0530

    Added jwt creating and verifying login

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
 const ApiRoutes = require("./routes/index");
+const UserService = require("./services/user-service");
 
 const app = express();
 
@@ -14,6 +15,18 @@ const setupAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
+
+    const userService = new UserService();
+    // const newToken = userService.createToken({
+    //   email: "kumar@admin.com",
+    //   id: 2,
+    // });
+    // console.log(newToken);
+
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imt1bWFyQGFkbWluLmNvbSIsImlkIjoyLCJpYXQiOjE2NzI2MDE5NjIsImV4cCI6MTY3MjYwNTU2Mn0.VBO6yr2cCTw9kROuYNXaX4mnSs8UsyHDlBsvwXYSFLk";
+    const response = userService.verifyToken(token);
+    console.log(response);
   });
 };
 
